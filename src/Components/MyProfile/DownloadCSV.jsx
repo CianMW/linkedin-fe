3ebc2e75bb1eef383c630bad98a473b3a2851691
CianMW <markwick.cian@gmail.com>
2021-11-19 00:17:32 +0000

    Activity css edit

--- src/Components/MyProfile/DownloadCSV.jsx
+++ src/Components/MyProfile/DownloadCSV.jsx
@@ -10,8 +10,8 @@ const DownloadCSV = ({user}) =>{
 
     return(
         <>
-        <a href={ process.env.REACT_APP_URL +`users/${user._id}/csv`} target="_blank">
-        <button className="profile-button">
+        <a id="removeTextDecoration" href={ process.env.REACT_APP_URL +`users/${user._id}/csv`} target="_blank">
+        <button className="d-flex changeTextSize profile-button">
         Download as CSV
         </button>
         </a>
